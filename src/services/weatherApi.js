a26505c7b9a44ee60f5f8b74e8cb0019d0f9700d
azerrors <azer.nagi.2005@gmail.com>
2023-11-23 01:27:38 +0400

    a

--- src/services/weatherApi.js
+++ src/services/weatherApi.js
@@ -1,7 +1,7 @@
 const KEY = "4306c0a7317483a94c9c55a4ce99b63a";
 const KEY_2 = "f11c76738c94466ab3b232410231811";
 const BASE_URL = "https://api.openweathermap.org/data/2.5/";
-const BASE_URL_2 = "http://api.weatherapi.com/v1";
+const BASE_URL_2 = "https://api.weatherapi.com/v1";
 // api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 
 export default async function getCurrentWeather(city , unit = "imperial") {
@@ -26,7 +26,7 @@ export async function getForecastWeather(city) {
     `${BASE_URL_2}/forecast.json?&key=${KEY_2}&q=${city}&days=7&aqi=yes&alerts=no`
   );
   const data = await res.json();
-  // console.log(data)
+  console.log(data)
   return data;
 }
 
