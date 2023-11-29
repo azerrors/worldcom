f35881edd7de51b0e46cc90eef0f0a886bb3329c
azerrors <azer.nagi.2005@gmail.com>
2023-11-29 23:18:48 +0400

    changing site icon

--- src/services/weatherApi.js
+++ src/services/weatherApi.js
@@ -1,7 +1,7 @@
-const KEY = "4306c0a7317483a94c9c55a4ce99b63a";
-const KEY_2 = "f11c76738c94466ab3b232410231811";
-const BASE_URL = "https://api.openweathermap.org/data/2.5/";
 const BASE_URL_2 = "https://api.weatherapi.com/v1";
+const KEY_2 = "f11c76738c94466ab3b232410231811";
+const KEY = "4306c0a7317483a94c9c55a4ce99b63a";
+const BASE_URL = "https://api.openweathermap.org/data/2.5/";
 
 export default async function getCurrentWeather(city, unit = "imperial") {
   const res = await fetch(
